34bb6e20a51dcb7d7552eab2f5e59c32e33d40b2
georgehughes <hughesgeorgem@gmail.com>
2024-12-01 14:52:04 +0100

    Day1 solutions part 1 and 2

---
+++ Day1HistorianHysteria.py
@@ -0,0 +1,9 @@
+from Solver import Solver
+
+
+class Day1HistorianHysteria(Solver):
+    def solvePart1(self, input_value: list[str]) -> int:
+        return sum([abs(int(x) - int(y)) for (x,y) in zip(sorted([l.split()[0] for l in input_value]), sorted([l.split()[1] for l in input_value]))])
+
+    def solvePart2(self, input_value: list[str]) -> int:
+        return sum([int(x) * sum([1 for i in [l.split()[1] for l in input_value] if x == i]) for x in [l.split()[0] for l in input_value]])
---
+++ Day1HistorianHysteriaTest.py
@@ -0,0 +1,12 @@
+import unittest
+
+from Day1HistorianHysteria import Day1HistorianHysteria
+from Solver import Solver
+from SolverTestBase import SolverTestBase
+
+
+class Day1HistorianHysteriaTest(SolverTestBase):
+    solverClass = Day1HistorianHysteria
+
+if __name__ == '__main__':
+    unittest.main()
