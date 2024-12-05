47ece39220e97a1001e18e1e0d6a09535cc19b89
georgehughes <hughesgeorgem@gmail.com>
2024-12-05 17:35:00 +0100

    Renamed incorrectly named test

--- Tests/Day4CeresSearchTest.py
+++ Tests/Day4CeresSearchTest.py
@@ -4,7 +4,7 @@ from Solvers.Day4CeresSearch import Day4CeresSearch
 from Tests.SolverTestBase import SolverTestBase
 
 
-class Day4CeresSearch(SolverTestBase):
+class Day4CeresSearchTest(SolverTestBase):
     solverClass = Day4CeresSearch
 
 if __name__ == '__main__':
