9806872f92db44970fce27d912c4621350604ed2
georgehughes <hughesgeorgem@gmail.com>
2024-12-07 18:42:54 +0100

    Day 7 Part 1 Solution

---
+++ Solvers/Day7BridgeRepair.py
@@ -0,0 +1,10 @@
+import re
+from functools import reduce
+
+
+class Day7BridgeRepair:
+    def solvePart1(self, input_value: list[str]) -> int:
+        return sum([(lambda result, values: result if result in [reduce(lambda v1, fv2: fv2[0](v1, fv2[1]), zip([lambda v1, v2: v1 + v2] + [(lambda v1, v2: v1 + v2) if (operator//(2**v))%2 else (lambda v1, v2: v1 * v2) for v in range(len(values)-1)], values), 0) for operator in range(2**(len(values)-1))] else 0)(*(lambda match: (int(match.group("Result")), [int(v) for v in match.group("Values").split()]))(re.search(r"(?P<Result>\d+): (?P<Values>(\d+ ?)+)", l))) for l in input_value])
+
+    def solvePart2(self, input_value: list[str]) -> int:
+        return 0
---
+++ Tests/Day7BridgeRepairTest.py
@@ -0,0 +1,11 @@
+import unittest
+
+from Solvers.Day7BridgeRepair import Day7BridgeRepair
+from Tests.SolverTestBase import SolverTestBase
+
+
+class Day7BridgeRepairTest(SolverTestBase):
+    solverClass = Day7BridgeRepair
+
+if __name__ == '__main__':
+    unittest.main()
