4f5a9ca4f44be6d3c286740ec6e4b1f9dc2e2f25
georgehughes <hughesgeorgem@gmail.com>
2024-12-09 20:43:50 +0100

    Day 8 Solutions

---
+++ Tests/Day8ResonantCollinearityTest.py
@@ -0,0 +1,11 @@
+import unittest
+
+from Solvers.Day8ResonantCollinearity import Day8ResonantCollinearity
+from Tests.SolverTestBase import SolverTestBase
+
+
+class Day8ResonantCollinearityTest(SolverTestBase):
+    solverClass = Day8ResonantCollinearity
+
+if __name__ == '__main__':
+    unittest.main()
