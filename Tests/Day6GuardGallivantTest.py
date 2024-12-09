37a6600808282204d353acb69fccf9c27a1bb0fa
georgehughes <hughesgeorgem@gmail.com>
2024-12-09 20:44:46 +0100

    Day 6 WIP

---
+++ Tests/Day6GuardGallivantTest.py
@@ -0,0 +1,11 @@
+import unittest
+
+from Solvers.Day6GuardGallivant import Day6GuardGallivant
+from Tests.SolverTestBase import SolverTestBase
+
+
+class Day6GuardGallivantTest(SolverTestBase):
+    solverClass = Day6GuardGallivant
+
+if __name__ == '__main__':
+    unittest.main()
