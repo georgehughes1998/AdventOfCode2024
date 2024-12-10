9b51ce17da72aaced063bd749c8535d570a10750
georgehughes <hughesgeorgem@gmail.com>
2024-12-10 18:22:15 +0100

    Day 10 Part 1 and 2 Solutions

---
+++ Tests/Day10HoofItTest.py
@@ -0,0 +1,11 @@
+import unittest
+
+from Solvers.Day10HoofIt import Day10HoofIt
+from Tests.SolverTestBase import SolverTestBase
+
+
+class Day10HoofItTest(SolverTestBase):
+    solverClass = Day10HoofIt
+
+if __name__ == '__main__':
+    unittest.main()
