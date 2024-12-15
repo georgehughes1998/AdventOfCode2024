88a8ff134bc647bbf9a273c434294f22516a0d7e
georgehughes <hughesgeorgem@gmail.com>
2024-12-15 19:36:04 +0100

    Day 15 Part 1 Solution

---
+++ Tests/Day15WarehouseWoesTest.py
@@ -0,0 +1,11 @@
+import unittest
+
+from Solvers.Day15WarehouseWoes import Day15WarehouseWoes
+from Tests.SolverTestBase import SolverTestBase
+
+
+class Day15WarehouseWoesTest(SolverTestBase):
+    solverClass = Day15WarehouseWoes
+
+if __name__ == '__main__':
+    unittest.main()
