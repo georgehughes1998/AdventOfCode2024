d8ddf7314b7d13217ff7f6e70ebf27f1c2cadadc
georgehughes <hughesgeorgem@gmail.com>
2024-12-25 16:16:53 +0100

    Day 17 Part 1 Solution

---
+++ Solvers/Day17ChronospatialComputer.py
@@ -0,0 +1,11 @@
+from itertools import combinations, takewhile
+from math import floor
+import re
+
+
+class Day17ChronospatialComputer:
+    def solvePart1(self, input_value: list[str], initialA = None, cutoff = False) -> str:
+        return (lambda combo: (lambda process_instruction : ",".join([str(v) for v in process_instruction(*(lambda s: (lambda state: (state[0], initialA if initialA != None else state[1], *state[2:]))((0, *[int(re.search(rf"Register {reg}: (?P<Value>\d+)", s).group("Value")) for reg in "ABC"], tuple([int(v) for v in re.search(r"Program: (?P<Program>(\d+,?)+)", s).group("Program").split(",")]))))("\n".join(input_value)))]))((process_instruction := lambda pointer, A, B, C, program, output=tuple() : (process_instruction(pointer + 2, floor(A / (2 ** combo(pointer, A, B, C, program))), B, C, program, output) if program[pointer] == 0 else process_instruction(pointer + 2, A, B ^ program[pointer + 1], C, program, output) if program[pointer] == 1 else process_instruction(pointer + 2, A, combo(pointer, A, B, C, program) % 8, C, program, output) if program[pointer] == 2 else (process_instruction(program[pointer + 1], A, B, C, program, output) if A != 0 else process_instruction(pointer + 2, A, B, C, program, output)) if program[pointer] == 3 else process_instruction(pointer + 2, A, B ^ C, C, program, output) if program[pointer] == 4 else process_instruction(pointer + 2, A, B, C, program, output + (combo(pointer, A, B, C, program) % 8,)) if program[pointer] == 5 else process_instruction(pointer + 2, A, floor(A / (2 ** combo(pointer, A, B, C, program))), C, program, output) if program[pointer] == 6 else process_instruction(pointer + 2, A, B, floor(A / (2 ** combo(pointer, A, B, C, program))), program, output) if program[pointer] == 7 else None) if pointer < len(program) - 1 and (not cutoff or program[:len(output)] == output) else output)))((lambda pointer, A, B, C, program: program[pointer + 1] if program[pointer + 1] in [0, 1, 2, 3] else A if program[pointer + 1] == 4 else B if program[pointer + 1] == 5 else C if program[pointer + 1] == 6 else None))
+
+    def solvePart2(self, input_value: list[str], testRange=4096) -> int:
+        return 0
---
+++ Tests/Day17ChronospatialComputerTest.py
@@ -0,0 +1,11 @@
+import unittest
+
+from Solvers.Day17ChronospatialComputer import Day17ChronospatialComputer
+from Tests.SolverTestBase import SolverTestBase
+
+
+class Day17ChronospatialComputerTest(SolverTestBase):
+    solverClass = Day17ChronospatialComputer
+
+if __name__ == '__main__':
+    unittest.main()
